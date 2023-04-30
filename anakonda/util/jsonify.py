8719eaec9dea32975d974bcf77c74241c48e3209
zahra-ehghaghi <z.ehghaghi@gmail.com>
2023-04-30 09:21:28 +0330

    update jsonify codes

--- anakonda/util/jsonify.py
+++ anakonda/util/jsonify.py
@@ -3,6 +3,14 @@ from anakonda.config import Config
 STATUS_MESSAGES = {
     100: "OK",
     101: "Method is not implemented",
+    102: "Schema validation faild",
+    103: "Schema instance error",
+    104: "Invalid Input is provided",
+    105: "Media type is not supported",
+    106: "Database error",
+    107: "Resource is not found",
+    108: "Runtime is not available",
+    109: "Resource is not updataable",
 }
 
 
